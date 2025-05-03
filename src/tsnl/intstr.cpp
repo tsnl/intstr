641d8dd1a1a710ac0e93eb3dc2b8a005921e6b50
Nikhil Idiculla <nikhilidiculla@gmail.com>
2025-05-03 01:23:23 -0700

    Init intstr atop cpp_base_main

---
+++ src/tsnl/intstr.cpp
@@ -0,0 +1,29 @@
+#include "tsnl/intstr.hpp"
+
+namespace tsnl::scheme {
+
+std::shared_mutex intstr::mutex_;
+std::unordered_map<std::string_view, uint32_t> intstr::s2i_map_;
+std::deque<std::string> intstr::i2s_tab_;
+
+auto intstr::intern(std::string_view str) -> uint32_t {
+    // Try looking up all interned strings with a read lock first:
+    {
+        std::shared_lock lock(mutex_);
+        auto it = s2i_map_.find(str);
+        if (it != s2i_map_.end()) {
+            return it->second;
+        }
+    }
+
+    // If not found, acquire a write lock to add the new string:
+    {
+        std::unique_lock lock(mutex_);
+        uint32_t index = i2s_tab_.size();
+        i2s_tab_.emplace_back(str);
+        s2i_map_[str] = index;
+        return index;
+    }
+}
+
+} // namespace tsnl::scheme
