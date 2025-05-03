32b1ede05dee5a40fcf80058b67298d2c0ffde83
Nikhil Idiculla <nikhilidiculla@gmail.com>
2025-05-03 01:23:23 -0700

    Init intstr atop cpp_base_main

---
+++ include/tsnl/intstr.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <deque>
+#include <shared_mutex>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+
+namespace tsnl::scheme {
+
+class intstr {
+public:
+    intstr() = delete;
+    intstr(intstr const&) = default;
+    intstr(intstr&&) = default;
+
+    auto operator=(intstr const&) -> intstr& = default;
+    auto operator=(intstr&&) -> intstr& = default;
+
+    inline explicit intstr(uint32_t index);
+    inline explicit operator uint32_t() const;
+
+    inline intstr(std::string_view str);
+    inline operator std::string() const;
+
+    [[nodiscard]] auto operator<=>(intstr const&) const = default;
+
+private:
+    static std::shared_mutex mutex_;
+    static std::unordered_map<std::string_view, uint32_t> s2i_map_;
+    static std::deque<std::string> i2s_tab_;
+
+    static auto intern(std::string_view str) -> uint32_t;
+
+    uint32_t index_;
+};
+
+inline intstr::intstr(uint32_t index)
+: index_(index) {
+}
+
+inline intstr::operator uint32_t() const {
+    return index_;
+}
+
+inline intstr::intstr(std::string_view str)
+: index_(intern(str)) {
+}
+
+inline intstr::operator std::string() const {
+    std::shared_lock lock(mutex_);
+    return i2s_tab_[index_];
+}
+
+} // namespace tsnl::scheme
---
+++ test/tsnl/test_intstr.cpp
@@ -0,0 +1,37 @@
+#include "tsnl/intstr.hpp"
+
+#include <gtest/gtest.h>
+
+namespace tsnl::scheme {
+
+class intstr_test : public ::testing::Test {
+protected:
+    intstr_test() = default;
+    ~intstr_test() override = default;
+};
+
+TEST_F(intstr_test, strong_ordering) {
+    intstr s1("Hello");
+    intstr s2("World");
+
+    EXPECT_EQ(s1, s1);
+    EXPECT_NE(s1, s2);
+    EXPECT_LT(s1, s2);
+}
+
+TEST_F(intstr_test, idempotence) {
+    intstr s1("Hello");
+    intstr s2("Hello");
+    intstr s3("World");
+
+    EXPECT_EQ(uint32_t(s1), uint32_t(s2));
+    EXPECT_NE(uint32_t(s1), uint32_t(s3));
+
+    EXPECT_EQ(std::string(s1), std::string(s2));
+    EXPECT_NE(std::string(s1), std::string(s3));
+
+    EXPECT_EQ(std::string(s1), "Hello");
+    EXPECT_EQ(std::string(s3), "World");
+}
+
+} // namespace tsnl::scheme
